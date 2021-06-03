994b67104fa2e909eb27100b639560253f796e8a
Kevin Wonus <sales@wonus.com>
2021-06-02 22:43:56 -0400

    Update spec and initialization

--- Magnanimity.cpp
+++ Magnanimity.cpp
@@ -11,9 +11,9 @@ static XVMem<UINT64> Magnaminous;
 
 //  For every server, client, listener, the fist 8 utf8 bytes MUST BE UNIQUE from one another
 //  Note: that name can be of all three modes (uniqueness is within each collection)
-static const UINT64* initialize(char* folder, char* memmap, char* servers[], char* listeners[], UINT64 requestCnt)
+static const UINT64* initialize(char* folder, char* memmap, char* listeners[], UINT64 requestCnt)
 {
-    if (servers == NULL || listeners == NULL || requestCnt == 0 || requestCnt == 0xFFFFFFFFFFFFFFFF)
+    if (listeners == NULL || requestCnt == 0 || requestCnt == 0xFFFFFFFFFFFFFFFF)
         return NULL;
     int len = folder != NULL ? strnlen(folder, MAX_PATH) : 1;
     if (len >= MAX_PATH-1 && len > 0)
@@ -24,48 +24,34 @@ static const UINT64* initialize(char* folder, char* memmap, char* servers[], cha
             return false;
         g_hSharedHome[len++] = '/';
     }
-    UINT64 serverCnt = 0;
     UINT64 listenerCnt = 0;
 
-    for (char** s = servers; *s != NULL; s++)
-        serverCnt++;
     for (char** l = listeners; *l != NULL; l++)
         listenerCnt++;
 
-    if (serverCnt == 0)
-        return NULL;
     if (listenerCnt == 0 || listenerCnt > 64)
         return NULL;
 
-    UINT64 slots = 3
+    UINT64 slots = 6
     + 2
-    + (serverCnt * 2)
     + (listenerCnt * 2)
-    + ((listenerCnt*requestCnt) * 3);
+    + (requestCnt * 3);
 
     auto magnaminous = Magnaminous.Acquire(memmap != NULL ? memmap : "magnaminous.data", true, true, slots, &zero, &zero, &zero);
 
     //  DIMENENSIONS: read-only [all but magnanimity]
-    magnaminous[0] = serverCnt;  // Sn
-    magnaminous[1] = listenerCnt;// Ln
-    magnaminous[2] = requestCnt; // Rn
-    magnaminous[3] = 0; // status-zero == initializing
-    magnaminous[4] = 0; // don't mark heartbeat until right before setting status to 1
+    magnaminous[0] = listenerCnt;// Ln
+    magnaminous[1] = requestCnt; // Rn
+    magnaminous[2] = 0; // watchdog: status-zero == initializing
+    magnaminous[3] = 0; // watchdog: don't mark heartbeat until right before setting status to 1
+    magnaminous[4] = 0; // watchdog: Initization completed 
+    magnaminous[5] = 0; // server: status-zero == initializing
+    magnaminous[6] = 0; // server: don't mark heartbeat until right before setting status to 1
+    magnaminous[7] = 0; // server: Initization completed 
 
-    auto position = 5;
+    auto position = 8;
 
-    //  HEARTBEATS: servers and listeners are expected to manage their own heartbeat with GetTickCount() or similar
-    for (char** item = servers; *item != NULL; item++) {
-        UINT64 encoded = 0;
-        char* e = (char*)&encoded;
-        char* incoming = *item;
-        for (int i = 0; *incoming && i < 8; i++)
-            *e++ = tolower(*incoming++);
-        if (encoded == 0)
-            return NULL;
-        magnaminous[position++] = encoded;
-        magnaminous[position++] = 0;    // server heartbeat
-    }
+    //  HEARTBEATS: listeners are expected to manage their own heartbeat with GetTickCount() or similar
     for (char** item = listeners; *item != NULL; item++) {
         UINT64 encoded = 0;
         char* e = (char*)&encoded;
@@ -76,31 +62,16 @@ static const UINT64* initialize(char* folder, char* memmap, char* servers[], cha
             return NULL;
         magnaminous[position++] = encoded;
         magnaminous[position++] = 0;    // listener heartbeat
+        magnaminous[position++] = 0;    // listener request time
+        magnaminous[position++] = 0;    // listener request ack time
     }
-    //  SERVERS with LISTENERS: read-only [all but magnanimity]
-    for (char** server = servers; *server != NULL; server++) {
-        UINT64 encoded = 0;
-        char* e = (char*)&encoded;
-        char* incoming = *server;
-        for (int i = 0; *incoming && i < 8; i++)
-            *e++ = tolower(*incoming++);
-        magnaminous[position++] = encoded;  // server [ 1 thru Ns]
 
-        for (char** listener = listeners; *listener != NULL; listener++) {
-            encoded = 0;
-            e = (char*)&encoded;
-            incoming = *listener;
-            for (int i = 0; *incoming && i < 8; i++)
-                *e++ = tolower(*incoming++);
-            magnaminous[position++] = encoded;  // listener [ 1 thru Nl]
-        }
-    }
     //  REQUESTS:
-    //  Ln*Rn // each with Listener gets Rn requests
+    //  Rn*3 // each with Listener gets Rn requests
     for (int c = 0; c < listenerCnt; c++) {
         for (int r = 0; r < requestCnt; r++) {
+            magnaminous[position++] = 0;  // listener-hash
             magnaminous[position++] = 0;  // md5 of request
-            magnaminous[position++] = 0;  // server-hash
             magnaminous[position++] = 0;  // server-response-timestamp
         }
     }
